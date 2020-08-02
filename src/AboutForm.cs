e4b312e64a2e6a199e5273e12db3e70d036e4c1b
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2020-08-02 16:18:42 +1100

    Initial commit (2.08.20)

---
+++ src/AboutForm.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace RD_AAOW
+	{
+	/// <summary>
+	/// Класс описывает интерфейс отображения сведений о программе
+	/// </summary>
+	public partial class AboutForm:Form
+		{
+		// Переменные
+		private string projectLink, updatesLink, userManualLink;
+		private SupportedLanguages al;
+		private string updatesMessage = "";
+
+		/// <summary>
+		/// Основная Git-ссылка
+		/// </summary>
+		public const string DefaultGitLink = "https://github.com/adslbarxatov/";
+
+		/// <summary>
+		/// Часть ссылки на раздел Git-обновлений
+		/// </summary>
+		public const string GitUpdatesSublink = "/releases";
+
+		/// <summary>
+		/// Конструктор. Запускает форму
+		/// </summary>
+		/// <param name="InterfaceLanguage">Язык интерфейса</param>
+		/// <param name="Description">Описание программы и/или справочная информация</param>
+		/// <param name="ProjectLink">Ссылка на страницу проекта;
+		/// кнопка отключается, если это значение не задано</param>
+		/// <param name="UpdatesLink">Ссылка на страницу обновлений;
+		/// кнопка отключается, если это значение не задано</param>
+		/// <param name="UserManualLink">Ссылка на страницу руководства пользователя;
+		/// кнопка отключается, если это значение не задано</param>
+		public AboutForm (SupportedLanguages InterfaceLanguage, string ProjectLink,
+			string UpdatesLink, string UserManualLink, string Description)
+			{
+			// Инициализация
+			InitializeComponent ();
+			al = InterfaceLanguage;
+
+			// Настройка контролов
+			switch (al)
+				{
+				case SupportedLanguages.ru_ru:
+					UserManualButton.Text = "Руководство";
+					ProjectPageButton.Text = "О проекте";
+					UpdatesPageButton.Text = "Обновления";
+					ADPButton.Text = "Политика и EULA";
+					AvailableUpdatesLabel.Text = "проверяются...";
+
+					this.Text = "О программе";
+					break;
+
+				default:	// en_us
+					UserManualButton.Text = "User manual";
+					ProjectPageButton.Text = "Project webpage";
+					UpdatesPageButton.Text = "Updates webpage";
+					ADPButton.Text = "Policy and EULA";
+					AvailableUpdatesLabel.Text = "checking...";
+
+					this.Text = "About application";
+					break;
+				}
+
+			// Получение параметров
+			userManualLink = ((UserManualLink == null) ? "" : UserManualLink);
+
+			if (ProjectLink == null)
+				projectLink = "";
+			else if (ProjectLink == "*")
+				projectLink = DefaultGitLink + ProgramDescription.AssemblyMainName;
+			else
+				projectLink = ProjectLink;
+
+			if (UpdatesLink == null)
+				updatesLink = "";
+			else if (UpdatesLink == "*")
+				updatesLink = DefaultGitLink + ProgramDescription.AssemblyMainName + GitUpdatesSublink;
+			else
+				updatesLink = UpdatesLink;
+
+			DescriptionBox.Text = ((Description == null) ? "" : Description);
+
+			// Загрузка окружения
+			AboutLabel.Text = ProgramDescription.AssemblyTitle + "\n" + ProgramDescription.AssemblyDescription + "\n\n" +
+				ProgramDescription.AssemblyCopyright + "\nv " + ProgramDescription.AssemblyVersion +
+				"; " + ProgramDescription.AssemblyLastUpdate;
+
+			IconBox.BackgroundImage = Icon.ExtractAssociatedIcon (Assembly.GetExecutingAssembly ().Location).ToBitmap ();
+			OtherIconBox.BackgroundImage = this.Icon.ToBitmap ();
+
+			// Завершение
+			UserManualButton.Enabled = (userManualLink != "");
+			ProjectPageButton.Enabled = (projectLink != "");
+			UpdatesPageButton.Enabled = (updatesLink != "");
+
+			// Запуск проверки обновлений
+			HardWorkExecutor hwe = new HardWorkExecutor (UpdatesChecker, null, "");
+			UpdatesTimer.Enabled = true;
+
+			// Запуск
+			this.ShowDialog ();
+			}
+
+		/// <summary>
+		/// Конструктор. Открывает указанную ссылку без запуска формы
+		/// </summary>
+		/// <param name="Link">Ссылка для отображения (если null, запускается стандартная)</param>
+		public AboutForm (string Link)
+			{
+			try
+				{
+				if (Link == null)
+					Process.Start (DefaultGitLink + ProgramDescription.AssemblyMainName + GitUpdatesSublink);
+				else
+					Process.Start (Link);
+				}
+			catch
+				{
+				}
+			}
+
+		// Закрытие окна
+		private void ExitButton_Click (object sender, EventArgs e)
+			{
+			this.Close ();
+			}
+
+		// Изменение размера окна
+		private void AboutForm_Resize (object sender, EventArgs e)
+			{
+			this.Width = this.MinimumSize.Width;
+
+			DescriptionBox.Height = this.Height - 225;
+			ExitButton.Top = this.Height - 63;
+			}
+
+		// Запуск ссылок
+		private void UserManualButton_Click (object sender, EventArgs e)
+			{
+			try
+				{
+				Process.Start (userManualLink);
+				}
+			catch
+				{
+				}
+			}
+
+		private void ProjectPageButton_Click (object sender, EventArgs e)
+			{
+			try
+				{
+				Process.Start (projectLink);
+				}
+			catch
+				{
+				}
+			}
+
+		private void UpdatesPageButton_Click (object sender, EventArgs e)
+			{
+			try
+				{
+				Process.Start (updatesLink);
+				}
+			catch
+				{
+				}
+			}
+
+		private void ADP_Click (object sender, EventArgs e)
+			{
+			try
+				{
+				Process.Start ("https://vk.com/@rdaaow_fupl-adp");
+				}
+			catch
+				{
+				}
+			}
+
+		// Метод-исполнитель проверки обновлений
+		private void UpdatesChecker (object sender, DoWorkEventArgs e)
+			{
+			// Настройка безопасности соединения
+			ServicePointManager.SecurityProtocol = (SecurityProtocolType)0xFC0;
+			// Принудительно открывает TLS1.0, TLS1.1 и TLS1.2; блокирует SSL3
+
+			// Запрос обновлений пакета
+			HttpWebRequest rq = (HttpWebRequest)WebRequest.Create (projectLink);
+			rq.Method = "GET";
+			rq.KeepAlive = false;
+			rq.Timeout = 10000;
+
+			// Отправка запроса
+			HttpWebResponse resp = null;
+			string html = "";
+			try
+				{
+				resp = (HttpWebResponse)rq.GetResponse ();
+				}
+			catch
+				{
+				goto htmlError;
+				}
+
+			// Чтение ответа
+			StreamReader SR = new StreamReader (resp.GetResponseStream ());
+			html = SR.ReadToEnd ();
+			SR.Close ();
+			resp.Close ();
+
+			// Разбор ответа (извлечение версий и PCC)
+			string version = "";
+			string[] htmlMarkers = { "</a>" + ProgramDescription.AssemblyMainName, "</h1>" };
+
+			int i = html.IndexOf (htmlMarkers[0]);
+			if (i < 0)
+				goto htmlError;
+
+			i += htmlMarkers[0].Length;
+
+			int j = html.IndexOf (htmlMarkers[1], i);
+			if ((j < 0) || (j <= i))
+				goto htmlError;
+
+			version = html.Substring (i, j - i).Trim ();
+
+			// Отображение результата
+			switch (al)
+				{
+				case SupportedLanguages.ru_ru:
+					if (ProgramDescription.AssemblyTitle.EndsWith (version))
+						updatesMessage = "обновлений нет";
+					else
+						updatesMessage = "доступна " + version;
+					break;
+
+				default:	// en_us
+					if (ProgramDescription.AssemblyTitle.EndsWith (version))
+						updatesMessage = "no updates";
+					else
+						updatesMessage = version + " available";
+					break;
+				}
+
+
+			e.Result = 0;
+			return;
+
+			// Есть проблема при загрузке страницы. Отмена
+htmlError:
+			switch (al)
+				{
+				case SupportedLanguages.ru_ru:
+					updatesMessage = "недоступны";
+					break;
+
+				default:	// en_us
+					updatesMessage = "unavailable";
+					break;
+				}
+
+			e.Result = -2;
+			return;
+			}
+
+		// Контроль сообщения об обновлении
+		private void UpdatesTimer_Tick (object sender, EventArgs e)
+			{
+			if (updatesMessage != "")
+				{
+				if (AvailableUpdatesLabel.Text == "")
+					{
+					AvailableUpdatesLabel.Text = updatesMessage;
+					if (!updatesMessage.Contains ("."))
+						{
+						UpdatesTimer.Enabled = false;
+						}
+					else
+						{
+						UpdatesTimer.Interval = 1000;
+						}
+					}
+				else
+					{
+					AvailableUpdatesLabel.Text = "";
+					}
+				}
+			}
+		}
+	}
---
+++ src/GMJNotifierForm.cs
@@ -0,0 +1,299 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RD_AAOW
+	{
+	/// <summary>
+	/// Класс описывает главную форму приложения
+	/// </summary>
+	public partial class GMJNotifierForm:Form
+		{
+		// Индикатор обновлений
+		private NotifyIcon ni = new NotifyIcon ();
+
+		private string currentPostID = "";
+		private uint currentOffset = 0;
+		private bool allowExit = false;
+		private string helpShownAt = "";
+
+		private string[] regParameters = new string[] { "Left", "Top", "Width", "Height", "Read", "HelpShownAt" };
+
+		/// <summary>
+		/// Конструктор. Настраивает главную форму приложения
+		/// </summary>
+		public GMJNotifierForm ()
+			{
+			// Инициализация
+			InitializeComponent ();
+			this.Text = ProgramDescription.AssemblyTitle;
+			this.CancelButton = BClose;
+			this.AcceptButton = BNext;
+			MainText.Font = new Font (SystemFonts.DialogFont.FontFamily.Name, 13);
+
+			// Получение настроек
+			try
+				{
+				this.Left = int.Parse (Registry.GetValue (ProgramDescription.AssemblySettingsKey, regParameters[0],
+					"").ToString ());
+				this.Top = int.Parse (Registry.GetValue (ProgramDescription.AssemblySettingsKey, regParameters[1],
+					"").ToString ());
+				this.Width = int.Parse (Registry.GetValue (ProgramDescription.AssemblySettingsKey, regParameters[2],
+					"").ToString ());
+				this.Height = int.Parse (Registry.GetValue (ProgramDescription.AssemblySettingsKey, regParameters[3],
+					"").ToString ());
+				this.ReadMode.Checked = bool.Parse (Registry.GetValue (ProgramDescription.AssemblySettingsKey, regParameters[4],
+					"").ToString ());
+				helpShownAt = Registry.GetValue (ProgramDescription.AssemblySettingsKey, regParameters[5],
+					"").ToString ();
+				}
+			catch
+				{
+				}
+			ReadMode_CheckedChanged (null, null);
+
+			// Настройка иконки в трее
+			ni.Icon = Properties.GMJNotifier.GMJNotifier16;
+			ni.Visible = true;
+
+			ni.ContextMenu = new ContextMenu ();
+			ni.ContextMenu.MenuItems.Add (new MenuItem ("Полный текст", ShowFullText));
+			ni.ContextMenu.MenuItems[0].DefaultItem = true;
+			ni.DoubleClick += ShowFullText;
+			ni.ContextMenu.MenuItems.Add (new MenuItem ("Следующий пост", UpdateMessages));
+			ni.ContextMenu.MenuItems.Add ("-");
+			ni.ContextMenu.MenuItems.Add (new MenuItem ("Перейти на страницу сообщества", GoToCommunity));
+			ni.ContextMenu.MenuItems.Add (new MenuItem ("О приложении", AboutService));
+			ni.ContextMenu.MenuItems.Add (new MenuItem ("Закрыть", CloseService));
+
+			// Запуск
+			UpdateMsg (currentOffset, false);
+			MainTimer.Interval = 30 * 60 * 1000;
+			MainTimer.Enabled = true;
+			}
+
+		private void GMJNotifierForm_Shown (object sender, EventArgs e)
+			{
+			this.Hide ();
+
+			if (helpShownAt != ProgramDescription.AssemblyVersion)
+				AboutService (null, null);
+			}
+
+		// Завершение работы службы
+		private void CloseService (object sender, EventArgs e)
+			{
+			allowExit = true;
+			this.Close ();
+			}
+
+		private void GMJNotifierForm_FormClosing (object sender, FormClosingEventArgs e)
+			{
+			// Остановка службы
+			if (allowExit)
+				{
+				// Остановка
+				ni.Visible = false;
+				MainTimer.Enabled = false;
+
+				// Сохранение настроек
+				try
+					{
+					Registry.SetValue (ProgramDescription.AssemblySettingsKey, regParameters[0], this.Left.ToString ());
+					Registry.SetValue (ProgramDescription.AssemblySettingsKey, regParameters[1], this.Top.ToString ());
+					Registry.SetValue (ProgramDescription.AssemblySettingsKey, regParameters[2], this.Width.ToString ());
+					Registry.SetValue (ProgramDescription.AssemblySettingsKey, regParameters[3], this.Height.ToString ());
+					Registry.SetValue (ProgramDescription.AssemblySettingsKey, regParameters[4], this.ReadMode.Checked.ToString ());
+					Registry.SetValue (ProgramDescription.AssemblySettingsKey, regParameters[5], ProgramDescription.AssemblyVersion);
+					}
+				catch
+					{
+					}
+				}
+
+			// Скрытие окна просмотра
+			else
+				{
+				this.Hide ();
+				e.Cancel = true;
+				}
+			}
+
+		// О приложении
+		private void AboutService (object sender, EventArgs e)
+			{
+			AboutForm af = new AboutForm (SupportedLanguages.ru_ru, "*", "*", "",
+				"Данная служба предоставляет возможность получать новые записи сообщества Grammar must joy в виде " +
+				"сообщений в трее.\r\n\r\n" +
+				"Контекстное меню, вызываемое по правому щелчку на значке приложения в трее, позволяет управлять " +
+				"просмотром постов, просматривать их в большом окне (в порядке от новых к старым) и запрашивать информацию " +
+				"о приложении.\r\n\r\nКроме того, приложение раз в " + (MainTimer.Interval / 60000).ToString () + " минут самостоятельно " +
+				"запрашивает новые записи и при их наличии отображает соответствующее оповещение.\r\n\r\n" +
+				"Будьте всегда веселы и бодры! Вместе в GMJ");
+			}
+
+		// Обновление сообщений
+		private void UpdateMessages (object sender, EventArgs e)
+			{
+			UpdateMsg (currentOffset, false);
+			}
+
+		private void MainTimer_Tick (object sender, EventArgs e)
+			{
+			UpdateMsg (0, false);
+			}
+
+		// Главный метод запроса сообщений
+		private void UpdateMsg (uint Offset, bool HideBallons)
+			{
+			// Настройка безопасности соединения
+			ServicePointManager.SecurityProtocol = (SecurityProtocolType)0xFC0;
+			// Принудительно открывает TLS1.0, TLS1.1 и TLS1.2; блокирует SSL3
+
+			// Запрос обновлений
+			HttpWebRequest rq = (HttpWebRequest)WebRequest.Create (string.Format (ProgramDescription.ArticleQuery, Offset));
+			rq.Method = "GET";
+			rq.KeepAlive = false;
+			rq.Timeout = 10000;
+
+			if (Offset == 0)
+				currentOffset = 1;
+			else
+				currentOffset++;
+
+			// Отправка запроса
+			HttpWebResponse resp = null;
+			string html = "";
+			try
+				{
+				resp = (HttpWebResponse)rq.GetResponse ();
+				}
+			catch
+				{
+				// Любая ошибка здесь будет означать необходимость прекращения проверки
+				ni.ShowBalloonTip (3000, "- " + ProgramDescription.AssemblyTitle + " -",
+					"\nСообщество Grammar must joy недоступно.\n\nПроверьте подключение к сети интернет", ToolTipIcon.Error);
+				return;
+				}
+
+			// Чтение ответа
+			StreamReader SR = new StreamReader (resp.GetResponseStream (), Encoding.UTF8);
+			html = SR.ReadToEnd ();
+			SR.Close ();
+			resp.Close ();
+
+			// Разбор ответа
+			int idLeft = 0, idRight = 0,
+				textLeft = 0, textRight = 0;
+			if (((idLeft = html.IndexOf ("id")) < 0) || ((idRight = html.IndexOf (",", idLeft + 4)) < 0) ||
+				((textLeft = html.IndexOf ("text")) < 0) || ((textRight = html.IndexOf ("\"", textLeft + 7)) < 0))
+				{
+				ni.ShowBalloonTip (3000, "- " + ProgramDescription.AssemblyTitle + " -",
+					"\nВозможно, мы достигли конца новостной ленты.\n\nПопробуем начать сначала", ToolTipIcon.Info);
+				currentOffset = 0;
+				return;
+				}
+
+			// Получение ID и текста
+			idLeft += 4;
+			textLeft += 7;
+
+			string id = html.Substring (idLeft, idRight - idLeft);
+			if (id == currentPostID)
+				return;
+
+			MainText.Text = html.Substring (textLeft, textRight - textLeft).Replace ("\\n", "\r\n").Replace ("\\/", "/").
+				Replace ("\\\\", "\\");
+			currentPostID = id;
+
+			if (MainText.Text.Length < 1)
+				{
+				MainText.Text = "\x01 Пост с номером " + id + " – это длинная история; откройте страницу сообщества, чтобы прочесть его";
+				ni.ShowBalloonTip (3000, "- " + ProgramDescription.AssemblyTitle + " -", "\n" + MainText.Text, ToolTipIcon.Info);
+				return;
+				}
+
+			// Отображение
+			if (!HideBallons)
+				ni.ShowBalloonTip (30000, "- " + ProgramDescription.AssemblyTitle + " -",
+					"\n" + (MainText.Text.Length > 200 ? MainText.Text.Substring (0, 200) + "..." : MainText.Text), ToolTipIcon.None);
+			}
+
+		// Отображение полного текста
+		private void ShowFullText (object sender, EventArgs e)
+			{
+			this.Show ();
+			CheckArticle ();
+			}
+
+		// Метод проверяет, следует ли открыть статью в браузере
+		private void CheckArticle ()
+			{
+			if (MainText.Text.StartsWith ("\x01") && (MessageBox.Show (this, "Открыть статью в браузере?",
+				ProgramDescription.AssemblyTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes))
+				{
+				try
+					{
+					Process.Start (ProgramDescription.ArticleLink + currentPostID);
+					}
+				catch
+					{
+					}
+				}
+			}
+
+		// Переход на страницу сообщества
+		private void GoToCommunity (object sender, EventArgs e)
+			{
+			try
+				{
+				Process.Start (ProgramDescription.MasterCommunityLink);
+				}
+			catch
+				{
+				}
+			}
+
+		// Изменение размера формы
+		private void GMJNotifierForm_Resize (object sender, EventArgs e)
+			{
+			MainText.Width = this.Width - 30;
+			MainText.Height = this.Height - 80;
+
+			BClose.Top = BNext.Top = ReadMode.Top = this.Height - 60;
+			}
+
+		// Закрытие окна просмотра
+		private void BClose_Click (object sender, EventArgs e)
+			{
+			this.Close ();
+			}
+
+		// Переход к следующему посту
+		private void BNext_Click (object sender, EventArgs e)
+			{
+			UpdateMsg (currentOffset, true);
+			CheckArticle ();
+			}
+
+		// Переход в режим чтения и обратно
+		private void ReadMode_CheckedChanged (object sender, EventArgs e)
+			{
+			if (ReadMode.Checked)
+				{
+				MainText.ForeColor = Color.FromArgb (163, 163, 163);
+				MainText.BackColor = Color.FromArgb (17, 17, 17);
+				}
+			else
+				{
+				MainText.ForeColor = Color.FromArgb (36, 36, 36);
+				MainText.BackColor = Color.FromArgb (255, 255, 255);
+				}
+			}
+		}
+	}
