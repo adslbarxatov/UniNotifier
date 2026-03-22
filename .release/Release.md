3607aae3723cca657cc8916ac99ed34076953a04
RD AAOW Free development lab <adslbarxatov@gmail.com>
2026-03-22 18:31:10 +1100

    Assembly v 8.9.5 (22.03.26)

--- .release/Release.md
+++ .release/Release.md
@@ -1,17 +1,15 @@
 _en_us_
 
+- Removed the ability to open the notification source by clicking on the balloon due to its instability;
 - Improved the updates checking method;
-- Updated the package format;
-- Offline help for this app has been discontinued;
-- Tray notification pop-ups are now clickable
+- Offline help for this app has been discontinued
 
 ⁂
 
 _ru_ru_
 
+- Изъята возможность перехода к источнику оповещения по нажатию на всплывающее уведомление из-за её нестабильности;
 - Улучшен метод проверки обновлений;
-- Обновлен формат пакета;
-- Автономная справка для этого приложения снята с поддержки;
-- Всплывающие уведомления в трее теперь кликабельны
+- Автономная справка для этого приложения снята с поддержки
 
 ⁂
--- src/UniNotifierForm.cs
+++ src/UniNotifierForm.cs
@@ -30,7 +30,7 @@ namespace RD_AAOW
 
 		// Индексы оповещений-источников в порядке, в котором они расположены в журнале
 		private List<int> notSenders = [];
-		private int currentNotificationForTip = -1;
+		/*private int currentNotificationForTip = -1;*/
 
 		// Флаг, указывающий на необходимость сворачивания окна в трей
 		private bool hideWindow;
@@ -98,7 +98,7 @@ namespace RD_AAOW
 			ni.Icon = UniNotifierResources.UniNotifierTrayN;
 			ni.Text = RDGenerics.DefaultAssemblyVisibleName;
 			ni.Visible = true;
-			ni.BalloonTipClicked += GoToLinkFromBalloon;
+			/*ni.BalloonTipClicked += GoToLinkFromBalloon;*/
 
 			ReloadTrayContextMenu ();
 			}
@@ -268,7 +268,7 @@ namespace RD_AAOW
 
 				texts.RemoveAt (0);
 				notSenders.Add (notNumbers[0]);
-				currentNotificationForTip = notNumbers[notNumbers.Count - 1];
+				/*currentNotificationForTip = notNumbers[notNumbers.Count - 1];*/
 				notNumbers.RemoveAt (0);
 				}
 
@@ -402,10 +402,10 @@ namespace RD_AAOW
 			GoToLinkByIndex (notContextMenu.Items.IndexOf ((ToolStripItem)sender));
 			}
 
-		private void GoToLinkFromBalloon (object sender, EventArgs e)
+		/*private void GoToLinkFromBalloon (object sender, EventArgs e)
 			{
 			GoToLinkByIndex (currentNotificationForTip);
-			}
+			}*/
 
 		private void GoToLinkByIndex (int Index)
 			{
